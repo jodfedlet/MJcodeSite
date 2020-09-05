46f692b87240e55da41c3e1a21c2a25ffdccd965
makender103 <makender103@gmail.com>
2020-09-05 20:39:50 -0300

    Our Work section

--- Includes/ourWork.php
+++ Includes/ourWork.php
@@ -9,51 +9,62 @@
           <div class="heading-underline"></div>
       </div>
 	  			<div align="center">
-				    <button class="btn btn-primary active filter-button" data-filter="todo">Toutes</button>
-				    <button class="btn btn-primary filter-button" data-filter="website">Website/Blog</button>
-				    <button class="btn btn-primary filter-button" data-filter="MobileApp">App Mobile</button>
-				    <button class="btn btn-primary filter-button" data-filter="ecommerce">Site E-commerce</button>
+				    <button class="btn btn-danger active filter-button" data-filter="todo">Toutes</button>
+				    <button class="btn btn-danger filter-button" data-filter="website">Website/Blog</button>
+				    <!-- <button class="btn btn-success filter-button" data-filter="MobileApp">App Mobile</button> -->
+				    <button class="btn btn-danger filter-button" data-filter="ecommerce">Site E-commerce</button>
 
 						<div class="row">
 							<div class="row">
-					            <div class="col-lg-3 col-md-4 col-xs-6 thumb filter website">
-					                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+					            <div class="col-lg-4 col-md-4 col-xs-6 thumb filter website">
+					                <a class="thumbnail" href="#" data-image-id="CashLakay" data-toggle="modal" data-title="CashLakay.com"
 					                   data-image="public/img/cashLakay.jpg"
 					                   data-target="#image-gallery">
 					                    <img class="img-thumbnail"
 					                         src="public/img/cashLakay.jpg"
 					                         alt="website">
 					                </a>
+                      </div>
+                      
+                      <div class="col-lg-4 col-md-4 col-xs-6 thumb filter website">
+                        <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="RVH.com"
+                          data-image="public/img/ONG1.jpg"
+                          data-target="#image-gallery">
+                          <img class="img-thumbnail"
+                                src="public/img/ONG.jpg"
+                                alt="website">
+                        </a>
 					            </div>
 					  
-					            <div class="col-lg-3 col-md-4 col-xs-6 thumb filter MobileApp">
-					                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
-					                   data-image="https://images.pexels.com/photos/1563286/pexels-photo-1563286.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-					                   data-target="#image-gallery">
+					            <div class="col-lg-4 col-md-4 col-xs-6 thumb filter ecommerce">
+					                <a class="thumbnail" href="http://www.nuvann.com" data-image-id="" data-toggle="modal" data-title="Nuvann.com"
+					                   data-image="public/img/Nuvann.jpg"
+														 data-target="#image-gallery">
+														 
 					                    <img class="img-thumbnail"
-					                         src="https://images.pexels.com/photos/1563286/pexels-photo-1563286.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+					                         src="public/img/Nuvann1.jpg"
 					                         alt="Paisaje">
 					                </a>
 					            </div>
 
-					        <div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					        <div class="modal fade " id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 					            <div class="modal-dialog modal-lg">
-					                <div class="modal-content">
-					                    <div class="modal-header">
-					                        <h4 class="modal-title" id="image-gallery-title"></h4>
-					                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span>
-					                        </button>
-					                    </div>
-					                    <div class="modal-body">
-					                        <img id="image-gallery-image" class="img-responsive col-md-12" src="">
-					                    </div>
-					                    <div class="modal-footer">
-					                        <button type="button" class="btn btn-secondary float-left" id="show-previous-image"><i class="fa fa-arrow-left"></i>
-					                        </button>
+					                <div class="modal-content portfolioModal">
+																<h4 class="modal-title" id="image-gallery-title"></h4>
+																<div class="modalHeader">
+																	<button type="button" class="close text-white" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span>
+																</div>
+																</button>
+														<div class="modal-body">
+																<img id="image-gallery-image" class="img-responsive col-md-12" src="">
+														</div>
+														<div class="modal-footer">
+																<button type="button" class="btn btn-danger float-left" id="show-previous-image"><i class="fa fa-arrow-left"></i>
+																</button>
 
-					                        <button type="button" id="show-next-image" class="btn btn-secondary float-right"><i class="fa fa-arrow-right"></i>
-					                        </button>
-					                    </div>
+																<button type="button" id="show-next-image" class="btn btn-danger float-right"><i class="fa fa-arrow-right"></i>
+																</button>
+														</div>
 					                </div>
 					            </div>
 					        </div>
